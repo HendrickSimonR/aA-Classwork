76ecefe7795036c7dfb61b831392d7d74e6afd35
David Chan <dchaan1@gmail.com>
2021-09-21 16:33:28 -0700

    Recent changes

--- w4/d2/class_inheritance/Manager.rb
+++ w4/d2/class_inheritance/Manager.rb
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 require_relative 'Employee'
-=======
-require_relative 'Employee.rb'
->>>>>>> 3c298e23404f6fe30b7e3109ddafa22660874fe5
-=======
-require_relative 'Employee.rb'
->>>>>>> 3c298e23404f6fe30b7e3109ddafa22660874fe5
 
 class Manager < Employee 
   attr_reader :employees
