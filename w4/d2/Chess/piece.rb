c3d57699770ed6beecf78718c321404bd5bbf46b
David Chan <dchaan1@gmail.com>
2021-09-22 10:50:25 -0700

    Created pieces classes

--- w4/d2/Chess/piece.rb
+++ w4/d2/Chess/piece.rb
@@ -1,4 +1,4 @@
-class Piece #SUPERCLASS
+class Piece
 
   def initialize(color, board, pos)
     @color = color # white/black
@@ -6,4 +6,27 @@ class Piece #SUPERCLASS
     @pos = pos # [x,y]
   end
 
+  def pos=(val)
+  end
+
+  def to_s
+  end
+
+  def empty?
+  end
+
+  def moves
+  end 
+end
+
+class Rook < Piece
+end
+
+class Knight < Piece
+end
+
+class Pawn < Piece
+end
+
+class NullPiece < Piece
 end
