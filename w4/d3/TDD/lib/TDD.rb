73b6a4a994f509a0327da9d85ba4c7277a4ed5f3
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-09-23 14:34:30 -0700

    Added Array#two_sum

--- w4/d3/TDD/lib/TDD.rb
+++ w4/d3/TDD/lib/TDD.rb
@@ -4,4 +4,13 @@ def my_uniq(array)
   uniques
 end 
 
+class Array
+  def two_sum
+    pairs = []
+    (0...self.length - 1).each do |i|
+      (i + 1...self.length).each {|j| pairs << [i, j] if self[i] + self[j] == 0} 
+    end
+    pairs
+  end
+end
 
--- w4/d3/TDD/spec/TDD_spec.rb
+++ w4/d3/TDD/spec/TDD_spec.rb
@@ -9,3 +9,15 @@ describe "#my_uniq" do
     expect(my_uniq([1, 2, 1, 3, 2, 3])).to eq([1, 2, 3])
   end 
 end 
+
+describe "Array" do
+  describe "#two_sum" do
+    it "should accept an array as an arg" do
+      expect{[-1, 0, 2, -2, 1].two_sum}.to_not raise_error
+    end
+
+    it "should return pairs of indices that sum to zero and pairs should be in order dictionary-wise" do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
+  end
+end
