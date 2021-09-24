453ddad3904fdb62cd22aae61f6b75200408edfd
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-09-24 18:04:59 -0400

    Create largest_contiguous_subsum method

--- w4/d5/execute.rb
+++ w4/d5/execute.rb
@@ -22,7 +22,63 @@ def my_min2(arr)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min2(list)
+# p my_min2(list)
 
 # linear polynomial, n
 
+
+def largest_contiguous_subsum(list)
+  sub_arrs = []
+
+  (0...list.length).each do |i| 
+    (0...list.length).each do |j|
+      sub_arrs << list[i..j]if i <= j
+    end 
+  end 
+
+  max = nil
+  sub_arrs.each do |sub|
+    max = sub.sum if max == nil || sub.sum > max
+  end 
+
+  max 
+end
+
+
+# Time Complexity = O(n^3) polynomial/quadratic
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
+
+    # possible sub-sums
+    # [5]           # => 5
+    # [5, 3]        # => 8 --> we want this one
+    # [5, 3, -7]    # => 1
+    # [3]           # => 3
+    # [3, -7]       # => -4
+    # [-7]          # => -7
+
+def largest_contiguous_subsum2(list)
+  largest_sum = list.first #5
+  current_sum = list.first # 0
+
+  (1...list.length).each do |i|
+    if list[i] + current_sum > largest_sum
+      largest_sum = list[i] + current_sum
+      p largest_sum
+      p "----------------"
+    else  
+      current_sum = 0
+      p current_sum
+      p "================"
+    end 
+  end 
+
+  largest_sum
+end
+
+
+
+
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum2(list) # => 8 (from [7, -6, 7])
