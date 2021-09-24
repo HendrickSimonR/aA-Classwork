df6aee81c12b575afe4b8b0827000184f79c4163
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-24 15:23:02 -0700

    Work on project

--- w4/d5/execute.rb
+++ w4/d5/execute.rb
@@ -58,27 +58,24 @@ end
     # [-7]          # => -7
 
 def largest_contiguous_subsum2(list)
-  largest_sum = list.first #5
-  current_sum = list.first # 0
+  largest_sum = list.first #2
+  current_sum = list.first #2
 
   (1...list.length).each do |i|
-    if list[i] + current_sum > largest_sum
-      largest_sum = list[i] + current_sum
-      p largest_sum
-      p "----------------"
-    else  
+    if current_sum < 0
       current_sum = 0
-      p current_sum
-      p "================"
     end 
+    current_sum += list[i]
+    if current_sum > largest_sum
+      largest_sum = current_sum
+    end
   end 
-
   largest_sum
 end
 
+# O(n) time because only used one loop
 
 
 
-
-list = [2, 3, -6, 7, -6, 7]
+list = [-5, -1, -3]
 p largest_contiguous_subsum2(list) # => 8 (from [7, -6, 7])
