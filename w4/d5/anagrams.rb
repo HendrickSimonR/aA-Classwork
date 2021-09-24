41abded6e6de741b6e70a5e4981033aad5f7fe7f
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-24 15:36:52 -0700

    Work on project

--- w4/d5/anagrams.rb
+++ w4/d5/anagrams.rb
@@ -1,10 +1,10 @@
 def first_anagram?(str1, str2)
-  anagrams = []
-  (0...str1.length).each do |i|
-    anagrams << str1.permutations(i)
-  end
-  anagrams
+  anagrams = str1.split('').permutation.to_a
+  split = str2.split('')
+  anagrams.include?(split)
 end
 
 p first_anagram?("gizmo", "sally")
-p first_anagram?("elvis", "lives") 
+p first_anagram?("elvis", "lives")
+
+# n factorial because it deafults to the greatest worst case scenario
