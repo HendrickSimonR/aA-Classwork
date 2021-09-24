6879e76b6642ac5359df70f4f33587e5121a420f
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-24 14:13:02 -0700

    Work on project

--- w4/d5/execute.rb
+++ w4/d5/execute.rb
@@ -1,13 +1,28 @@
 def my_min(arr)
   smallest = nil
-  arr.each.with_index do |num1, idx|
+  arr.each do |num1|
     smallest = num1 if arr.all? do |num2|
-      num1 < num2
-      p smallest
+      num1 <= num2
     end
   end
   smallest
 end
 
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)
+
+# quadratic polynomial, n^2
+
+def my_min2(arr)
+  smallest = nil
+  arr.each do |num1|
+    smallest = num1 if smallest.nil? || smallest > num1
+  end
+  smallest
+end
+
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)
+p my_min2(list)
+
+# linear polynomial, n
+
