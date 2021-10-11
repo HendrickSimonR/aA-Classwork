8529e1d28309aa0c35573c8ddeaccc4f279239f7
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-10-11 18:20:37 -0400

    Finish users model

--- w7/d1/99Cats/app/models/user.rb
+++ w7/d1/99Cats/app/models/user.rb
@@ -1,2 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true 
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true 
+  validates :session_token, presence: true, uniqueness: true 
+
+  after_initialize :ensure_session_token
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    
+    if user && user.is_valid_password?(password)
+      return user
+    else
+      nil
+    end 
+  end
+
+  def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
+    @password = pw
+  end
+
+  def is_valid_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end 
 end
