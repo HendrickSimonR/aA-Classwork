8c1ac29e0d415aa3728df02685af81e145377caa
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-10-19 19:55:15 -0400

    Create myMap function

--- w8/d1/Intro to JS/phase_2_enumerable.js
+++ w8/d1/Intro to JS/phase_2_enumerable.js
@@ -1,17 +1,37 @@
 Array.prototype.myEach = function(cb) {
+  for (let i = 0; i <= this.length -1; i++) {
+    cb(this[i]);
+  }
+}
+
+// let arr = [1, 3, 5, 7, 9, 11];
+// let cb = function (num) {
+//   return num + 1;
+// }
+
+// console.log(arr.myEach(cb));
+
+
+Array.prototype.myMap = function (cb) {
   let newArr = [];
 
-  for (let i = 0; i <= this.length -1; i++) {
-    newArr.push(cb(this[i]));
-  }
+  this.myEach( function(num) {
+      newArr.push(cb(num));
+    }
+  )
 
   return newArr;
 }
 
+
 let arr = [1, 3, 5, 7, 9, 11];
 let cb = function (num) {
-  return num + 1;
+  return num += 1;
 }
 
-console.log(arr.myEach(cb));
+// console.log(arr);
+// console.log(arr.myEach(cb));
+// console.log(arr);
+
+console.log(arr.myMap(cb))
 
