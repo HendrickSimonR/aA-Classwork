5b573132e4e35aa3d33f2c1e7d5b49a4c96a19ae
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-19 17:35:46 -0700

    finish day [ar]

--- w8/d1/Intro to JS/phase_2_enumerable.js
+++ w8/d1/Intro to JS/phase_2_enumerable.js
@@ -24,14 +24,41 @@ Array.prototype.myMap = function (cb) {
 }
 
 
-let arr = [1, 3, 5, 7, 9, 11];
-let cb = function (num) {
-  return num += 1;
-}
+
+// let cb = function (num) {
+//   return num += 1;
+// }
 
 // console.log(arr);
 // console.log(arr.myEach(cb));
 // console.log(arr);
 
-console.log(arr.myMap(cb))
+// console.log(arr.myMap(cb))
+
+
+Array.prototype.myReduce = function(acc, iv=this[0]) {
+  let sum = 0
+  if (iv !== this[0]) {
+    sum = iv;
+  } else if (iv === this[0]) {
+    sum = 0;
+  }
+
+  this.myEach(function (acc, iv=this[0]) {
+    sum += acc
+  }
+  )
+
+  return sum;
+}
+
+let arr = [1, 3, 5, 7, 9, 11];
+
+console.log([1, 2, 3].myReduce(function (acc, el) {
+  return acc + el;
+}));
+
+console.log([1, 2, 3].myReduce(function (acc, el) {
+  return acc + el;
+}, 25));
 
