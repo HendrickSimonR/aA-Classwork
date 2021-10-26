07fd0d18649c1d268bc2f8317ce5196ffbea76a4
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-10-26 17:38:01 -0400

    Create setupBoard function

--- w9/d2/Browser TicTacToe/src/index.js
+++ w9/d2/Browser TicTacToe/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("../ttt_node/game.js");
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+  const el = document.querySelector(".ttt");
+  const view = new View(game, el);
 });
--- w9/d2/Browser TicTacToe/src/ttt-view.js
+++ w9/d2/Browser TicTacToe/src/ttt-view.js
@@ -1,7 +1,24 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard;
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const grid = document.createElement("ul");
+
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+      for (let colIdx = 0; colIdx < 3; colIdx++) {
+        let cell = document.createElement("li");
+        cell.dataset.pos = JSON.stringify([rowIdx, colIdx]);
+
+        grid.appendChild(cell);
+      }
+    }
+
+    this.el.append(grid);
+  }
   
   bindEvents() {}
 
